c5e75aebe5bd05e46c56825c3606e5cd95ff2ece
Your Name <uzcointg@gmail.com>
2022-10-28 20:26:32 +0500

    some changes

---
+++ js/splide.js
@@ -0,0 +1,11 @@
+new Splide( '.products__slider', {
+    perPage: 4,
+    gap: '10px',
+    pagination: false,
+}).mount();
+
+new Splide( '.slider', {
+    perPage: 1,
+    perMove: 1,
+    gap: '3rem'
+}).mount();
