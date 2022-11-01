5888d89b7451a9dcedfa3cf052fcdb4065567102
Your Name <uzcointg@gmail.com>
2022-11-01 10:27:16 +0500

    some changes

---
+++ js/card.js
@@ -0,0 +1,16 @@
+let splides = document.querySelectorAll(".card_img_slider");
+if (splides.length) {
+    let splideOptions = {
+        perPage: 1,
+        perMove: 1,
+        arrows: false,
+        autoplay: true,
+        lazyLoad: "nearby",
+        resetProgress: true,
+    };
+    for (let i = 0; i < splides.length; i++) {
+        let splideElement = splides[i];
+        let splide = new Splide(splideElement, splideOptions);
+        splide.mount();
+    }
+}
