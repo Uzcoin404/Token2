992866a5b9cfb656be9f6cc7f74b303a5e4398c3
Your Name <uzcointg@gmail.com>
2022-11-05 15:19:22 +0500

    fixed header bugs

--- js/login.js
+++ js/login.js
@@ -2,12 +2,10 @@ const login = document.querySelector('.after__login');
 const loginBtn = document.querySelector('.after__login').querySelector('.loginBtn');
 const verify = document.querySelector('.verify');
 
-
 function Login(e) {
     e.preventDefault();
     login.classList.add('d-none')
     verify.classList.remove('d-none')
 }
 
-
-loginBtn.addEventListener('click', Login)
+loginBtn.addEventListener('click', Login);
